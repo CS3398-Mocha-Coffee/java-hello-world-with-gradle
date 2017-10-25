ed32dde22cc2e93d81f86304eca9f9826b06bb35
Brian Fordyce <bmf57@txstate.edu>
2017-10-25 18:22:55 -0500

    Error Undone

--- src/test/java/testhello/TestGreeter.java
+++ src/test/java/testhello/TestGreeter.java
@@ -23,7 +23,7 @@ public class TestGreeter {
    public void testGreeterEmpty()
    {
       assertEquals(g.getName(),"");
-      assertEquals(g.sayHello(),"Hello hell...o?");
+      assertEquals(g.sayHello(),"Hello!");
    }
 
    @Test
