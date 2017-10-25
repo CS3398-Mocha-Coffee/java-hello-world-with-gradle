4b3d4620b1c609b9a58ac92b0b47a048768fc3ac
Sarah <sfg11@txstate.edu>
2017-10-25 17:53:19 -0500

    changed yml and testerGreeter

--- test/java/testhello/TestGreeter.java
+++ test/java/testhello/TestGreeter.java
@@ -13,22 +13,22 @@ import org.junit.Test;
 public class TestGreeter {
 
    private Greeter g;
-	
+
    @Before
-   public void setUp() throws Exception 
+   public void setUp() throws Exception
    {
       g = new Greeter();
    }
 
    @Test
-   public void testGreeterEmpty() 
+   public void testGreeterEmpty()
    {
       assertEquals(g.getName(),"");
-      assertEquals(g.sayHello(),"Hello!");
+      assertEquals(g.sayHello(),"Hello from Sarah!");
    }
-	
+
    @Test
-   public void testGreeter() 
+   public void testGreeter()
    {
       g.setName("World War");
       assertEquals(g.getName(),"World War");
